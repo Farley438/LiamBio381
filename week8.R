4275d4f9ca410ecdf66e80fbd7f7dd6f2216268e
unknown <farley742@gmail.com>
2020-04-08 15:13:44 -0400

    homework5

---
+++ week8.R
@@ -0,0 +1,17 @@
+# Priincples of structured programming
+# feb 27
+
+#############--------------
+
+# pseudocode
+# get data
+# calculate stuff
+# summarize stuff
+# graph shit
+
+# code will look like this:
+
+# get_data()
+# calculate_stuff()
+# summarize_output()
+# graph_results()
